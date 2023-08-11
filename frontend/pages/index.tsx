5136add6b7fa2f08c63fe3da73c641523f4af381
Leonardo Florentino <leonardo.marcao@unesp.br>
2023-08-11 18:26:04 -0300

    Makes last changes

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -4,7 +4,7 @@ import ImportsList from "@/pages/components/ImportsList";
 
 export default function Home() {
     return (
-        <div className="container max-w-2xl">
+        <div className="container mx-auto">
             <ImportsList/>
         </div>
     )
